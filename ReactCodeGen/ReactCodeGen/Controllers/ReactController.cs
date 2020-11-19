0d9b7f07353945c977369a7cec05ec8d5bc82025
RichikGupta24 <68510993+RichikGupta24@users.noreply.github.com>
2020-11-19 18:29:47 +0530

    Add files via upload
    Adding ReactCodeGen API.

---
+++ ReactCodeGen/ReactCodeGen/Controllers/ReactController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReactCodeGen.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ReactCodeGen.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ReactController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/reactCodeGen")]
+        public async Task<IActionResult> Get()
+        {
+            return StatusCode(200, "Welcome to React CodeGen API");
+        }
+
+        [HttpPost]
+        [Route("/reactCodeGen/api/retrieveAPIInfo")]
+        public IActionResult RetrieveAPIInformation([FromBody] string value)
+        {
+            var apiInfo = RetrieveAPIInfo(value);
+            return StatusCode(200,apiInfo);
+        }
+
+        private List<APIInfo> RetrieveAPIInfo(string value)
+        {
+            // This will be replaced by actual open api spec parsing code
+
+            return new List<APIInfo>()
+            {
+                new APIInfo
+                {
+                    APIName = "AddPet",
+                    APIUrl = "/pet",
+                    APIDesc = "Add a new pet to the store",
+                    APIType = "POST",
+                    TemplateType = "Create",
+                    Fields = new List<Field>
+                    {
+                        new Field
+                        {
+                            Name="name",
+                            ControlType="label"
+                        },
+                        new Field
+                        {
+                            Name="status",
+                            ControlType="dropdown",
+                            Values=new List<string>{"Available","Pending","Sold"}
+                        }
+                    }
+                }
+            };            
+        }
+    }
+}
---
+++ ReactCodeGen/ReactCodeGen/Models/Field.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactCodeGen.Models
+{
+    public class Field
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public List<string> Values { get; set; }
+        public string ControlType { get; set; }
+    }
+}
