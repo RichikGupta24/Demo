014455e343228a7f542a0bc9b98f95a74db688d8
naveensivaraju <30657596+naveensivaraju@users.noreply.github.com>
2020-11-23 09:21:23 +0530

    YamlDotNet console
    app that reads the Open Api spec file and created the dotnet dictionary object

---
+++ YamlDotNetConsole/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using YamlDotNet.Serialization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<object, object> yamlObject;
+            //using(var reader = new StreamReader(@"C:\Users\Naveen\Desktop\UI from Open API Doc\Petstore.yml"))
+            using (var reader = new StreamReader(@"C:\Users\Naveen\Desktop\UI from Open API Doc\openapi.json"))
+            {
+                var deserializer = new Deserializer();
+                yamlObject = deserializer.Deserialize<dynamic>(reader.ReadToEnd());
+            }
+
+            if (yamlObject != null)
+            {
+                foreach (var item in yamlObject)
+                {
+                    // code here
+                }
+            }
+        }
+    }
+}
